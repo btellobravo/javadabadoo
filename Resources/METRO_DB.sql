7cd4eaeac5656c093290d8a776677dd269324b27
Daniel Ortiz <danielortiz@MacBook-Pro-de-Daniel-4.local>
2019-09-30 22:14:04 -0500

    sql complete

---
+++ Resources/METRO_DB.sql
@@ -0,0 +1,97 @@
+DROP TABLE "Influx_Metro_2010";
+DROP TABLE "Influx_Metro_2011";
+DROP TABLE "Influx_Metro_2012";
+DROP TABLE "Influx_Metro_2013";
+DROP TABLE "Influx_Metro_2014";
+DROP TABLE "Influx_Metro_2015";
+DROP TABLE "Influx_Metro_2016";
+DROP TABLE "Influx_Metro_2017";
+DROP TABLE "Influx_Metro_2018";
+DROP TABLE "Influx_Metro_2019";
+
+CREATE TABLE "Influx_Metro_2010" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+
+SELECT * FROM "Influx_Metro_2010";
+
+
+CREATE TABLE "Influx_Metro_2011" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+SELECT * FROM "Influx_Metro_2011";
+
+CREATE TABLE "Influx_Metro_2012" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+
+SELECT * FROM "Influx_Metro_2012";
+
+CREATE TABLE "Influx_Metro_2013" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+
+SELECT * FROM "Influx_Metro_2013"; 
+
+CREATE TABLE "Influx_Metro_2014" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+     
+
+SELECT * FROM "Influx_Metro_2014";
+
+CREATE TABLE "Influx_Metro_2015" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+SELECT * FROM "Influx_Metro_2015";
+
+CREATE TABLE "Influx_Metro_2016" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+
+SELECT * FROM "Influx_Metro_2016";
+
+CREATE TABLE "Influx_Metro_2017" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+SELECT * FROM "Influx_Metro_2017";
+
+CREATE TABLE "Influx_Metro_2018" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+SELECT * FROM "Influx_Metro_2018";
+
+CREATE TABLE "Influx_Metro_2019" (
+  Year integer NOT NULL,
+  Line character varying(10) NOT NULL,
+  Station character varying(45) NOT NULL,
+  Influx integer NOT NULL
+);
+SELECT * FROM "Influx_Metro_2019";
