802469459ca3de60d35bf3fb915132190ce8b2db
Daniel Ortiz <danielortiz@MacBook-Pro-de-Daniel-4.local>
2019-10-02 19:54:12 -0500

    sql updated and incomplete data frame

--- Resources/METRO_DB.sql
+++ Resources/METRO_DB.sql
@@ -8,6 +8,8 @@ DROP TABLE "Influx_Metro_2016";
 DROP TABLE "Influx_Metro_2017";
 DROP TABLE "Influx_Metro_2018";
 DROP TABLE "Influx_Metro_2019";
+DROP TABLE "Unique Stations";
+DROP TABLE "Influx and coordinates";
 
 CREATE TABLE "Influx_Metro_2010" (
   Year integer NOT NULL,
@@ -95,3 +97,26 @@ CREATE TABLE "Influx_Metro_2019" (
   Influx integer NOT NULL
 );
 SELECT * FROM "Influx_Metro_2019";
+
+CREATE TABLE "Unique Stations" (
+  Station character varying(45) NOT NULL,
+  Latitude numeric NOT NULL,
+	Longitude numeric NOT NULL
+);
+SELECT * FROM "Unique Stations";
+
+
+CREATE TABLE "Influx and coordinates" (
+	Station character varying(45) NOT NULL,
+	Total_influx_2012 numeric NOT NULL,
+	Total_influx_2013 numeric NOT NULL,
+	Total_influx_2014 numeric NOT NULL,
+	Total_influx_2015 numeric NOT NULL,
+	Total_influx_2016 numeric NOT NULL,
+	Total_influx_2017 numeric NOT NULL,
+	Total_influx_2018 numeric NOT NULL,
+	Total_influx_2019 numeric NOT NULL,
+	Latitude numeric NOT NULL,
+	Longitude numeric NOT NULL
+);
+SELECT * FROM "Influx and coordinates";
